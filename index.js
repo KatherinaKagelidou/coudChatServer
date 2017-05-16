227565635f5489a67ec60ee1463e1beaf24d9ef1
Katherina Kagelidou <katherina.kagelidou@web.de>
2017-05-16 22:03:07 +0200

    redis update

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var server = require('http').createServer(app);
-
 var io = require('socket.io').listen(server, {transports: ['websocket']});
-
-
 var Cloudant = require('cloudant');
-//var redis = require('redis');
 var redis = require('redis');
 var cloudant;
-//var cfenv = require('cfenv');
-//var appEnv = cfenv.getAppEnv();
-//var cfEnv = require('cf-env');
-
 
 //use port 8080 unless there exists a preconfigured port
 var port = process.env.port || 8080;
@@ -22,21 +14,16 @@ var port = process.env.port || 8080;
 var username = "73bb44a7-c8d7-475f-9d79-d9c018c81f6c-bluemix", 
 	password = "34e4b92c18c2664dc54c6a8c7e9be8c1b978ad7a4ae8a44a5c6de0b615566cfd", 	
 	cloudant = Cloudant({
-
 	account : username,
 	password : password
-
 });
 
-
-users = {};
-
+var users = {};
 
 app.enable('trust proxy');
 
 //get path always to https 
 app.use(function(req, res, next) {
-
 	if (req.secure) {
 		next();
 	} else {
@@ -44,69 +31,55 @@ app.use(function(req, res, next) {
 	}
 });
 
-
-
 app.use('/', express.static(__dirname));
 
 app.get('/', function(req, res) {
-
 	res.sendfile('index.html');
-
 });
-
 
 init();
 
 //redis connection with out port, hostname and password
-var client = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com', {no_ready_check: true});
-client.auth('Qo49Fq6cSoqQzgYS', function (err) {
-  if (err) throw err;
-});
+//var client = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com', {no_ready_check: true});
+//client.auth('Qo49Fq6cSoqQzgYS', function (err) {
+//  if (err) throw err;
+//});
+//
+//client.on('connect', function() {
+//  console.log('Connected to Redis');
+//});
 
-client.on('connect', function() {
-  console.log('Connected to Redis');
-});
+var client1 = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com');
+ client1.auth('Qo49Fq6cSoqQzgYS');
 
-
-var subscriber = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com');
-subscriber.on("error", function(err) {
-  console.error('There was an error with the redis client ' + err);
-});
-var publisher = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com');
-publisher.on("error", function(err) {
-  console.error('There was an error with the redis client ' + err);
-});
-
-  subscriber.auth('Qo49Fq6cSoqQzgYS');
-  publisher.auth('Qo49Fq6cSoqQzgYS');
+var client2 = redis.createClient(17981, 'pub-redis-17981.dal-05.1.sl.garantiadata.com');
+client2.auth('Qo49Fq6cSoqQzgYS');
 
 
-subscriber.on('message', function(channel, msg) {
-	  if(channel === 'chatter') {
-   // while(clients.length > 0) {
-	      var client = clients.pop();
-	      client.end(msg);
-  //  }
-	  }
+client1.on('message', function(chan, msg) {
+	
+	client2.hgetall(msg, function(err, res){
+		
+		res.key=msg;
+		io.sockets.emit('send message',res);
+	});
+	
 	});
 
-	subscriber.subscribe('chatter');
-	
+	client1.subscribe('chatter');
+
+
 
 io.sockets.on('connection', function(socket) {
-
 	socket.on('new user', function(data, callback) {
-
-	
 		socket.on('setPassword', function(password) {
-			//if data is in araay
+			//if data is in array
 			if (data in users) {
 				//get the user (if exist)in the database with this password 
 				//if the user doesnt exist -->err else get the data 
 				database.get(password, function(err, body) {
 					console.log("Error:", err);
 					console.log("Data:", body);
-					
 					if (err) {
 						callback(false);
 					} else {
@@ -116,10 +89,8 @@ io.sockets.on('connection', function(socket) {
 						updateNicknames();
 						console.log("Welcome back " + data);
 						socket.emit('login', socket.nickname);
-
 					}
 				});
-
 			} else {
 				//get the user (if exist)in the database ant not in the array with this password 
 				//if the user doesnt exist insert him/her into the database
@@ -127,7 +98,7 @@ io.sockets.on('connection', function(socket) {
 					console.log("Error:", err);
 					console.log("Data:", body);
 					if (err) {
-						console.log("try it again");
+						console.log("New user " + data)
 						database.insert({
 							_id : data,
 							password : password
@@ -146,24 +117,18 @@ io.sockets.on('connection', function(socket) {
 						updateNicknames();
 						console.log("Welcome back " + socket.nickname);
 						socket.emit('login', socket.nickname);
-
 					}
 				});
-
 			}
 		});
-		
 	});
-
 
 	//this function is called to refresh the user array
 	function updateNicknames() {
 		io.sockets.emit('usernames', Object.keys(users));
 	}
 
-
 	socket.on('send message', function(data, callback) {
-
 		//ignore white space
 		var msg = data.trim();
 		//substr 0 for /, 1 for p, 3 for empty space
@@ -182,7 +147,6 @@ io.sockets.on('connection', function(socket) {
 					});
 					console.log('private message is going out by '
 							+ socket.nickname);
-			
 				} else {
 					callback('Enter a valid user');
 				}
@@ -191,31 +155,24 @@ io.sockets.on('connection', function(socket) {
 			}
 		} else {
 			//message for everyone
-			
 			io.sockets.emit('new message', {
 				msg : msg,
 				nick : socket.nickname
 			});
-			
-			publisher.publish("chatter", JSON.stringify(data));
-		}
 		
+			client1.publish("chatter", data);
+		}
 	});
 
-	
 	socket.on('disconnect', function(data) {
 		if (!socket.nickname)
 			return;
 		delete users[socket.nickname];
 		updateNicknames();
 	});
-
 });
 
-
-
 	function init() {
-
 	cloudant.db.create('chatserver', function() {
 		database = cloudant.db.use('chatserver');
 		if (database === undefined) {
@@ -225,7 +182,6 @@ io.sockets.on('connection', function(socket) {
 		}
 	});
 }
-	
 	
 	server.listen(port, function(){
 		console.log('Server listening on port ' + port);
